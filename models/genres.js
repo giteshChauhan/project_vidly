d75df4dfde53a214fe1f2069a82efd8e3b5fa65a
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-06-12 22:59:27 +0530

    added genres model

---
+++ models/genres.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const genreSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minlength:5,
+        maxlength:50
+    }
+});
+
+const Genre = mongoose.model('Genre',genreSchema);
+
+function validateGenre(name){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    return schema.validate(name);
+}
+
+exports.genreSchema = genreSchema;
+exports.Genre = Genre;
+exports.validate = validateGenre;
