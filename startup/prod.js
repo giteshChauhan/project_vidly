9b0d7ae702d8c1e1cecbfe836b9aaf059bd9e471
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-06-12 23:57:55 +0530

    updated compress to compression

--- startup/prod.js
+++ startup/prod.js
@@ -1,7 +1,7 @@
 const helmet = require('helmet');
-const compress = require('compress');
+const compress = require('compression');
 
 module.exports = function(app){
     app.use(helmet());
-    app.use(compress());
+    app.use(compression());
 }
