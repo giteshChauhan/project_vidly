a3d142da85a95cbc765dc5b9ebc81599def8d88f
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-08 19:47:20 +0530

    updated prod.js | added cors middleware

--- startup/prod.js
+++ startup/prod.js
@@ -1,7 +1,9 @@
-const helmet = require('helmet');
-const compression = require('compression');
+const helmet = require("helmet");
+const compression = require("compression");
+const cors = reqiure("cors");
 
-module.exports = function(app){
-    app.use(helmet());
-    app.use(compression());
-}
+module.exports = function (app) {
+  app.use(helmet());
+  app.use(compression());
+  app.use(cors());
+};
