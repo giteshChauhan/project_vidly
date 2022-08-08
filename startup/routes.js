3d93ca761fd42ebbabeaed7824685bf27f3af4d7
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-08 21:02:21 +0530

    updated routes.js | added cors code

--- startup/routes.js
+++ startup/routes.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const genres = require("../routes/genres");
+const customers = require("../routes/customers");
+const movies = require("../routes/movies");
+const rentals = require("../routes/rentals");
+const users = require("../routes/users");
+const login = require("../routes/login");
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const error = require("../middleware/error");
 
-const genres = require('../routes/genres');
-const customers = require('../routes/customers');
-const movies = require('../routes/movies');
-const rentals = require('../routes/rentals');
-const users = require('../routes/users');
-const login = require('../routes/login');
-const auth = require('../middleware/auth');
-const admin = require('../middleware/admin');
-const error = require('../middleware/error');
-
-
-module.exports = function (app){
-    app.use(express.json());
-    app.use(express.urlencoded({extended:true}));
-    app.use('/api/genres',[auth, admin],genres);
-    app.use('/api/customers',customers);
-    app.use('/api/movies',[auth, admin],movies);
-    app.use('/api/rentals',auth,rentals);
-    app.use('/api/users',users);
-    app.use('/api/login',login);
-app.use(error);
-}
+module.exports = function (app) {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
+  app.use("/api/genres", [auth, admin], genres);
+  app.use("/api/customers", customers);
+  app.use("/api/movies", [auth, admin], movies);
+  app.use("/api/rentals", auth, rentals);
+  app.use("/api/users", users);
+  app.use("/api/login", login);
+  app.use(error);
+};
