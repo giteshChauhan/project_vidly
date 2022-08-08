0a7e5547b6d3d55cc145d53e40284f3f8de923cc
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-08 21:02:00 +0530

    updated prod.js | deleted cors code

--- startup/prod.js
+++ startup/prod.js
@@ -1,9 +1,7 @@
 const helmet = require("helmet");
 const compression = require("compression");
-const cors = require("cors");
 
 module.exports = function (app) {
   app.use(helmet());
   app.use(compression());
-  app.use(cors());
 };
