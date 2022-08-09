3d6893a477325cb26e9e5d52ea43e0d97c3c261d
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-09 19:37:52 +0530

    updated moveis.js | JSON circular error fixed

--- routes/movies.js
+++ routes/movies.js
@@ -45,16 +45,17 @@ router.put("/:id", async (req, res) => {
   const { error } = validate(req.body);
   if (error) res.status(400).send(error.details[0].message);
 
-  const newGenre = await Genre.findById(
-    mongoose.Types.ObjectId(req.body.genreId)
-  );
-  if (!newGenre) res.status(400).send("Invalid genre ID");
+  const genre = await Genre.findById(mongoose.Types.ObjectId(req.body.genreId));
+  if (!genre) res.status(400).send("Invalid genre ID");
 
   const movie = await Movies.findByIdAndUpdate(
     mongoose.Types.ObjectId(req.params.id),
     {
       title: req.body.title,
-      genre: newGenre,
+      genre: {
+        _id: genre._id,
+        name: genre.name,
+      },
       numberInStock: req.body.numberInStock,
       dailyRentalRate: req.body.dailyRentalRate,
     },
