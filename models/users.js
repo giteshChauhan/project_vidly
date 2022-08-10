113b9d3ce443dbb719d9afa82059daa565e55df3
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-10 14:22:23 +0530

    updated user.js model | add name & email in token

--- models/users.js
+++ models/users.js
@@ -1,46 +1,54 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const passwordComplexity = require('joi-password-complexity');
-const mongoose = require('mongoose');
-const Joi = require('joi');
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const passwordComplexity = require("joi-password-complexity");
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 50
-    },
-    email:{
-        type: String,
-        required:true,
-        unique: true,
-        minlength: 11,
-        maxlength: 255
-    },
-    password:{
-        type: String,
-        required:true,
-        minlength: 8,
-        maxlength: 1024
-    },
-    isAdmin: {type:Boolean, default:false}
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 11,
+    maxlength: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 1024,
+  },
+  isAdmin: { type: Boolean, default: false },
 });
 
-userSchema.methods.generateAuthToken = function(){
-    const token = jwt.sign({_id: this._id, isAdmin: this.isAdmin},config.get('jwtPrivateKey'));
-    return token;
-}
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+      isAdmin: this.isAdmin,
+    },
+    config.get("jwtPrivateKey")
+  );
+  return token;
+};
 
-const Users = mongoose.model('user',userSchema);
+const Users = mongoose.model("user", userSchema);
 
-function validateUser(user){
-    const schema = Joi.object({
-        name: Joi.string().required().min(3).max(50),
-        email: Joi.string().required().min(11).max(255).email(),
-        password: passwordComplexity().required()
-    });
-    return schema.validate(user);
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().required().min(3).max(50),
+    email: Joi.string().required().min(11).max(255).email(),
+    password: passwordComplexity().required(),
+  });
+  return schema.validate(user);
 }
 
 exports.validate = validateUser;
