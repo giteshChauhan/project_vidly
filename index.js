bf3b5cdd7b572d9994d756d0eab273cf35e00943
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-14 01:50:53 +0530

    updated process port

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const {logger} = require('./logger/');
-const express = require('express');
+const { logger } = require("./logger/");
+const express = require("express");
 const app = express();
 
-require('./startup/handler')();
-require('./startup/routes')(app);
-require('./startup/db')();
-require('./startup/config')();
-require('./startup/validation')();
-require('./startup/prod')(app);
+require("./startup/handler")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
+require("./startup/prod")(app);
 
-let port = process.env.PORT || 3000;
-app.listen(port,() => logger.info(`Listening on port ${port}......`));
+let port = process.env.PORT || 3900;
+app.listen(port, () => logger.info(`Listening on port ${port}......`));
