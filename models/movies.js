216316f30d81e0cfd326209fe761e6556014e69d
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-16 15:36:24 +0530

    updated movies models

--- models/movies.js
+++ models/movies.js
@@ -1,24 +1,30 @@
-const mongoose = require('mongoose');
-const Joi = require('joi');
-const {genreSchema} = require('./genres');
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const { genreSchema } = require("./genres");
 
 const movieSchema = new mongoose.Schema({
-    title: {type:String,minlength:3,maxlength:30,required:true,unique:true},
-    genre: {type:genreSchema,required:true},
-    numberInStock:{type:Number,required:true,min:0,max:1000},
-    dailyRentalRate:{type:Number,required:true,min:0,max:255}
+  title: {
+    type: String,
+    minlength: 3,
+    maxlength: 30,
+    required: true,
+    unique: true,
+  },
+  genre: { type: genreSchema, required: true },
+  numberInStock: { type: Number, required: true, min: 0, max: 1000 },
+  dailyRentalRate: { type: Number, required: true, min: 0, max: 255 },
 });
 
-const Movies = mongoose.model('movie',movieSchema);
+const Movies = mongoose.model("movie", movieSchema);
 
-function validateMovie(movie){
-    const schema = Joi.object({
-        title: Joi.string().min(3).max(30).required(),
-        genreId: Joi.objectId().required(),
-        numberInStock: Joi.number().min(0).required(),
-        dailyRentalRate: Joi.number().min(0).required()
-    });
-    return schema.validate(movie);
+function validateMovie(movie) {
+  const schema = Joi.object({
+    title: Joi.string().min(3).max(30).required(),
+    genreId: Joi.objectId().required(),
+    numberInStock: Joi.number().min(0).required(),
+    dailyRentalRate: Joi.number().min(0).required(),
+  });
+  return schema.validate(movie);
 }
 
 exports.Movies = Movies;
