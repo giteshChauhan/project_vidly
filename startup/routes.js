85574e435ddabb8fa46603e05549846d06420945
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-09-05 07:42:17 +0530

    updated routes.js | adds cinema & conentType route

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const genres = require("../routes/genres");
+const cinema = require("../routes/cinema");
+const contentType = require("../routes/contentType");
 const customers = require("../routes/customers");
 const movies = require("../routes/movies");
 const rentals = require("../routes/rentals");
@@ -16,6 +18,8 @@ module.exports = function (app) {
   app.use(express.urlencoded({ extended: true }));
   app.use(allowCors());
   app.use("/api/genres", genres);
+  app.use("/api/cinema", cinema);
+  app.use("/api/contentType", contentType);
   app.use("/api/customers", [auth, admin], customers);
   app.use("/api/movies", movies);
   app.use("/api/rentals", auth, rentals);
