30e92bf144ea083002880bfa132a6dbf779df150
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-09-05 07:41:22 +0530

    updated movies.js model | add contentType & cinema

--- models/movies.js
+++ models/movies.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 const { genreSchema } = require("./genres");
+const { cinemaSchema } = require("./cinema");
+const { contentTypeSchema } = require("./contentType");
 
 const movieSchema = new mongoose.Schema({
   title: {
@@ -11,11 +13,12 @@ const movieSchema = new mongoose.Schema({
     unique: true,
   },
   genre: { type: genreSchema, required: true },
+  cinema: { type: cinemaSchema, required: true },
+  contentType: { type: contentTypeSchema, required: true },
   year: { type: Number, required: true, min: 1950, max: 2024 },
   rating: { type: Number, required: true, min: 0, max: 10 },
   yt_id: { type: String, required: true, length: 11 },
   imdb_id: { type: String, required: true },
-  contentType: { type: String, required: true },
 });
 
 const Movies = mongoose.model("movie", movieSchema);
@@ -24,11 +27,12 @@ function validateMovie(movie) {
   const schema = Joi.object({
     title: Joi.string().min(3).max(30).required(),
     genreId: Joi.objectId().required(),
+    cinemaId: Joi.objectId().required(),
+    contentTypeId: Joi.objectId().required(),
     rating: Joi.number().min(0).max(10).required(),
     year: Joi.number().min(1950).max(2024).required(),
     yt_id: Joi.string().length(11).required(),
     imdb_id: Joi.string().min(9).max(10).required(),
-    contentType: Joi.string().required(),
   });
   return schema.validate(movie);
 }
