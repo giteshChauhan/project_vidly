d4cebe8a39c0d1224f82b9d4689c29ac54e77bb2
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-09-07 20:38:53 +0530

    updated movies route| added thumbnailUrl

--- routes/movies.js
+++ routes/movies.js
@@ -17,7 +17,7 @@ router.get("/:id", [auth, admin], async (req, res) => {
   if (!mongoose.isValidObjectId(req.params.id))
     return res.status(400).send("Invalid Id");
   const movie = await Movies.findById(req.params.id).select(
-    "title year rating genre.name genre._id yt_id imdb_id contentType._id cinema._id contentType.name cinema.name -_id"
+    "title year rating genre.name genre._id yt_id imdb_id contentType._id cinema._id contentType.name cinema.name thumbnailUrl -_id"
   );
   if (!movie) return res.send("No such ID exists");
   res.send(movie);
@@ -50,6 +50,7 @@ router.post("/", [auth, admin], async (req, res) => {
       _id: contentType._id,
       name: contentType.name,
     },
+    thumbnailUrl: req.body.thumbnailUrl,
     year: req.body.year,
     rating: req.body.rating,
     yt_id: req.body.yt_id,
@@ -94,6 +95,7 @@ router.put("/:id", [auth, admin], async (req, res) => {
         _id: cinema._id,
         name: cinema.name,
       },
+      thumbnailUrl: req.body.thumbnailUrl,
       rating: req.body.rating,
       year: req.body.year,
       yt_id: req.body.yt_id,
